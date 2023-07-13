ac4b8556d38ec21133224400a54a2d76734838e9
Pasindu Sampath <104705509+pasindusampath@users.noreply.github.com>
2023-07-14 02:16:21 +0530

    Added Details View

--- assert/script/main.js
+++ assert/script/main.js
@@ -1,10 +1,14 @@
 class Main {
-    mobile;
+    mobile = '0787149579';
+
     constructor() {
+        localStorage.setItem('key', '64b043b1956013.46105348')
         $('#getOtp').click(this.getOtp.bind(this));
         $('#submitOtp').click(this.submitOtp.bind(this));
         $('#getData').click(this.getData.bind(this));
+        $('#btn600').click(this.request1.bind(this));
     }
+
     getOtp() {
         $('#getOtp').prop('disabled', true);
         let mn = $('#mobileNo').val();
@@ -16,6 +20,7 @@ class Main {
         this.mobile = mn
         this.request(mn);
     }
+
     submitOtp() {
         let pass = $('#password').val();
         if (!this.validatePassword()) {
@@ -23,14 +28,17 @@ class Main {
         }
         this.subOtp(pass);
     }
+
     validatePassword() {
         return (/^\d{5}$/).test($('#password').val())
     }
+
     validateMobile() {
         let mn = $('#mobileNo').val();
         let s = /^(?:0|94|\+94|0094)?(?:78\d|72\d)\d{6}$/;
         return s.test(mn)
     }
+
     subOtp(pass) {
         var settings = {
             "url": "https://oneapp.hutch.lk/hutch_2_0/index.php?r=scapp/connection/registerConfirmPin",
@@ -48,15 +56,16 @@ class Main {
         };
         $.ajax(settings).done(function (response) {
             console.log(response);
-            if(((JSON.parse(response)).success)===true){
+            if (((JSON.parse(response)).success) === true) {
                 console.log('OK');
             }
             console.log(((JSON.parse(response)).data).deviceToken);
-            localStorage.setItem('key',((JSON.parse(response)).data).deviceToken)
+            localStorage.setItem('key', ((JSON.parse(response)).data).deviceToken)
             $('.mainSection').removeClass('view');
             $('#getDataSection').addClass('view2');
         });
     }
+
     request(mn) {
         var settings = {
             "url": "https://oneapp.hutch.lk/hutch_2_0/index.php?r=scapp/connection/registerPinSend",
@@ -78,12 +87,11 @@ class Main {
             $('#getOtp').prop('disabled', false);
         })
     }
-    getData(){
-        setTimeout(function(){
-            $('#getData').prop('disabled',false);
-        }, 30000);
+
+    getData() {
+        this.btnTimeout('#getData');
         let item = localStorage.getItem('key');
-        let sent ="appType=android&appVersion=3.0.8&deviceModel=SM-G988N&deviceRef=ad13e41df99ff787&deviceVersion=7.1.2&platformName=android&platformVersion=7.1.2&deviceToken="+item+"&operator=HUTCH&lob=mobile&conn="+this.mobile.slice(1)+"&primaryConn="+this.mobile.slice(1)+"&prePostType=pre&language=en&pushId=fd4CkQQrTKSOC0Rh5rps-A%3AAPA91bEBRjwH9Vnhdjn62B44ZBk5KkeDyka90-MrhUxOMzeKyRvB4qfrAK7AazxJYzxb94qxFBKbFQRZ0J_blI9o1H0bZP11hDAIM1M8UELtN7SulyZ_-WiD7abGnK3glAt1EeVxsN_g&provider=gms&cosMerge=ID&id=3580&category=Selfcare&offerType=stv&price=0.0&name=25MB%20Data";
+        let sent = "appType=android&appVersion=3.0.8&deviceModel=SM-G988N&deviceRef=ad13e41df99ff787&deviceVersion=7.1.2&platformName=android&platformVersion=7.1.2&deviceToken=" + item + "&operator=HUTCH&lob=mobile&conn=" + this.mobile.slice(1) + "&primaryConn=" + this.mobile.slice(1) + "&prePostType=pre&language=en&pushId=fd4CkQQrTKSOC0Rh5rps-A%3AAPA91bEBRjwH9Vnhdjn62B44ZBk5KkeDyka90-MrhUxOMzeKyRvB4qfrAK7AazxJYzxb94qxFBKbFQRZ0J_blI9o1H0bZP11hDAIM1M8UELtN7SulyZ_-WiD7abGnK3glAt1EeVxsN_g&provider=gms&cosMerge=ID&id=3580&category=Selfcare&offerType=stv&price=0.0&name=25MB%20Data";
         var settings = {
             "url": "https://oneapp.hutch.lk/hutch_2_0/index.php?r=scapp/flyTextOffers/activateLoyaltyOffers",
             "method": "POST",
@@ -96,8 +104,36 @@ class Main {
             "data": sent,
         };
         $.ajax(settings).done(function (response) {
+            alert()
+        });
+    }
+
+    request1() {
+        this.btnTimeout('#btn600');
+        let data= localStorage.getItem('key');
+        var settings = {
+            "url": "https://oneapp.hutch.lk/hutch_2_0/index.php?r=scapp/flyTextOffers/activateLoyaltyOffers",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "Accept": "application/json",
+                "Accept-Encoding": "gzip",
+            },
+            "data": "appType=android&appVersion=3.0.8&deviceModel=SM-G988N&deviceRef=ad13e41df99ff787&deviceVersion=7.1.2&platformName=android&platformVersion=7.1.2&deviceToken="+data+"&operator=HUTCH&lob=mobile&conn="+this.mobile.slice(1)+"&primaryConn="+this.mobile.slice(1)+"&prePostType=pre&language=en&pushId=fd4CkQQrTKSOC0Rh5rps-A%3AAPA91bEBRjwH9Vnhdjn62B44ZBk5KkeDyka90-MrhUxOMzeKyRvB4qfrAK7AazxJYzxb94qxFBKbFQRZ0J_blI9o1H0bZP11hDAIM1M8UELtN7SulyZ_-WiD7abGnK3glAt1EeVxsN_g&provider=gms&cosMerge=ID&id=3741&category=Selfcare&offerType=stv&price=0.0&name=FREE%20600MB%20Offer",
+        };
+
+        $.ajax(settings).done(function (response) {
             console.log(response);
         });
     }
+
+    btnTimeout(btn){
+        $(btn).prop('disabled', true);
+        setTimeout(function() {
+            $(btn).prop('disabled', false);
+        }, 10000);
+    }
 }
+
 new Main();
